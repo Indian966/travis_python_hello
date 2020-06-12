8469d9f8704d345c1f8833f57a21086cfd3e3655
Indian966 <rlaalsrb4175@gmail.com>
2020-06-12 22:14:12 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 def main():
 	print(get_helloworld())
 
+def test_get_helloworld():
+	assert 'hello world' == get_helloworld()
+
 def get_helloworld():
-	print('hello world')
+	return 'hello world'
 
 if __name__ == '__main__':
 	main()
