aca2fb0daa7296ae7376e6018e34fed10cfeba4c
Indian966 <rlaalsrb4175@gmail.com>
2020-06-12 22:10:48 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def test_say_hello (a) :
-	assert a == 10, 'Test fail'
-	print("Test success")
+def main():
+	print(get_helloworld())
 
-test_say_hello(a = 10)
+def get_helloworld():
+	print('hello world')
+
+if __name__ == '__main__':
+	main()
